31c63a52709ca9a98a2f40b534615af08dc2bb85
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-10 14:26:16 +0200

    add animal table and it's data and queiry over the data

---
+++ data.sql
@@ -0,0 +1,9 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals 
+(name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES
+('Agumon', '20200203', 0, true, 10.23),
+('Gabumon', '20181115', 2, true, 8),
+('Pikachu', '20210107', 1, false, 15.04),
+('Devimon', '20170512', 5, true, 11);
