156b593b1a1483ea5e4b820b2c5e39da8d21fb93
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-12-12 17:24:19 +0200

    Add invoices and many to many relationship tables, and FK index for invoices table medical history

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -25,13 +25,44 @@ CREATE TABLE treatments (
   name VARCHAR
 );
 
+-- Table for Invoices 
+CREATE TABLE invoices (
+  id INT NOT NULL PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  FOREIGN KEY (medical_history_id)
+  REFERENCES medical_histories(id)
+  ON DELETE CASCADE
+);
+
 -- Table for invoice items
 CREATE TABLE invoice_items (
   id INT NOT NULL PRIMARY KEY,
   unit_price DECIMAL,
   quantity INT,
   total_price DECIMAL,
-  invoice_id INT REFERENCES invoices(id),
-  treatment_id INT REFERENCES treatments(id)
+  invoice_id INT,
+  treatment_id INT REFERENCES treatments(id),
+  FOREIGN KEY (invoice_id)
+  REFERENCES invoices(id)
+  ON DELETE CASCADE
 );
 
+-- Join many to many relationship table
+CREATE TABLE medical_histories_treatments (
+  id INT NOT NULL Primary KEY,
+  medical_history_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  FOREIGN KEY (medical_history_id) 
+  REFERENCES medical_histories (id) 
+  ON DELETE CASCADE,
+  FOREIGN KEY (treatment_id) 
+  REFERENCES treatments (id) 
+  ON DELETE CASCADE 
+);
+
+-- CREATE FK INDEXES
+CREATE INDEX mh_index
+ON invoices (medical_history_id);
